3759a5fd08c6f2396e6aee670cd2cbdb6dea32ec
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-11-12 08:53:29 +0900

    Generalizing the entire utils

--- utils.py
+++ utils.py
@@ -1,7 +1,30 @@
+import os
 from contextlib import nullcontext
 from pathlib import Path
+from typing import Union
 
 from tqdm import tqdm
+
+
+def assert_dir(path: Union[Path, str], name: str):
+    path = pathify(path)
+
+    assert path.exists(), f"{name} not found."
+    assert path.is_dir(), f"{name} must be a directory."
+    assert os.access(path, os.R_OK), f"{name} not readable."
+
+
+def assert_file(path: Union[Path, str], name: str, ext: str = None):
+    path = pathify(path)
+
+    assert path.exists(), f"{name} not found."
+    assert path.is_file(), f"{name} must be a file."
+    assert os.access(path, os.R_OK), f"{name} not readable."
+
+    if ext is not None:
+        ext = correct_suffix(ext)
+
+        assert path.suffix == ext, f"{name} must be in a {ext} format."
 
 
 def iterate(path: Path, operation, extension=None, progress_bar=True, single=False):
@@ -26,10 +49,21 @@ def iterate(path: Path, operation, extension=None, progress_bar=True, single=Fal
                 break
 
 
-def count_files(path: Path, recursive=True, extension=None):
+def count_files(path: Union[Path, str], recursive=True, ext: str = None):
+    assert_dir(path)
+
+    path = pathify(path)
     pattern = "**/*" if recursive else "*"
 
-    if extension is not None:
-        pattern += "." + extension
+    if ext is not None:
+        pattern += correct_suffix(ext)
 
-    return sum(1 for f in path.glob(pattern) if f.is_file())
+    return sum(1 for f in path.glob(pattern))
+
+
+def correct_suffix(suffix: str) -> str:
+    return suffix if suffix.startswith(".") else "." + suffix
+
+
+def pathify(path: Union[Path, str]):
+    return Path(path) if type(path) is str else path

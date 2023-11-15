de32ab76684d9f540de1e3420c2e2892404b99d9
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-11-16 08:35:36 +0900

    Added supported image formats

--- image_utils.py
+++ image_utils.py
@@ -8,6 +8,28 @@ def load_image_dir(path: Union[Path, str], flag: int = cv2.IMREAD_COLOR):
     assert_dir(path)
 
     path = Path(path)
+    supported_formats = (
+        ".bmp",
+        ".jpeg",
+        ".jpg",
+        ".png",
+        ".tiff",
+        ".tif",
+        ".gif",
+        ".webp",
+        ".jp2",
+        ".j2k",
+        ".pbm",
+        ".pgm",
+        ".ppm",
+        ".sr",
+        ".ras",
+        ".jpeg2000",
+        ".exr",
+    )
 
     for file in path.iterdir():
+        if not file.is_file or not file.suffix in supported_formats:
+            continue
+
         yield cv2.imread(str(file), flag)

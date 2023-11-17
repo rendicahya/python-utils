b28fa247692b23d5da1602aa00f5e26a827028e7
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-11-17 14:46:50 +0900

    Renamed submodule name

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from utils.config import Config
-from utils.file import assert_dir, assert_file, correct_suffix, count_files, iterate
-from utils.image import load_image_dir
-from utils.video import frames_to_video, get_frames, video_info, video_writer_like
+from python_utils.config import Config
+from python_utils.file import assert_dir, assert_file, correct_suffix, count_files, iterate
+from python_utils.image import load_image_dir
+from python_utils.video import frames_to_video, get_frames, video_info, video_writer_like
